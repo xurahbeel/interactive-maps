1cbcf90a240da1150365580a927aa24469b49f94
Shurahbeel Khan <tell2xb@gmail.com>
2026-03-25 07:07:28 +0500

    deployemnt

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/interactive-maps',
+  images: {
+    unoptimized: true,
+  },
   transpilePackages: ["mapbox-gl"],
 };
 
